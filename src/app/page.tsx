766ee8c09ba2eccf0531fba12cadf0efe32b0eaf
Jay Ho Oh <ohjayhoi@gmail.com>
2024-09-22 18:29:03 +0900

    refactor: render categories using map to reduce redundancy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
 import CardsCarousel from "@/components/CardsCarousel";
 
 export default function Home() {
+  const categories = ["Transportation", "Debit", "City Pass"];
   return (
     <>
-      <div className="w-full h-full flex justify-center items-center">
-        <div>
+      <div className="w-full h-full flex justify-center pt-[20%] bg-[url('/bg.jpg')] bg-cover bg-center">
+        <div className="flex flex-col items-center">
           <h1 className="text-4xl font-bold">Transportation Cards in Korea</h1>
-          <section className="flex justify-between">
-            <div>
-              <h3>Transportation</h3>
-            </div>
-            <div>
-              <h3>Debit</h3>
-            </div>
-            <div>
-              <h3>City Pass</h3>
-            </div>
+          <section className="flex justify-between w-full h-16">
+            {categories.map((category) => (
+              <div className="flex items-center" key={category}>
+                <h3>{category}</h3>
+              </div>
+            ))}
           </section>
           <CardsCarousel />
         </div>

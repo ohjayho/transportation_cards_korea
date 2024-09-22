a53b3714bcdf6ddb004068ae6b8832d495185fc0
Jay Ho Oh <ohjayhoi@gmail.com>
2024-09-22 18:29:56 +0900

    feat: implement 3d carousel

--- src/components/CardsCarousel.tsx
+++ src/components/CardsCarousel.tsx
@@ -1,5 +1,7 @@
-import { promises as fs } from "fs";
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 type Card = {
   category: Array<string>;
@@ -8,32 +10,96 @@ type Card = {
   img: string;
 };
 
-export default async function CardsCarousel() {
-  const cardsFile = await fs.readFile(
-    process.cwd() + "/public/card_info/card.json",
-    "utf8"
-  );
-  const cards = JSON.parse(cardsFile);
-  const numCards = cards.length;
+export default function CardsCarousel() {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [carouselAngle, setCarouselAngle] = useState(0);
+  const [radius, setRadius] = useState(0);
+  const width = 210;
+  const [theta, setTheta] = useState(0);
+  const [cardAngle, setCardAngle] = useState(0);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/cards");
+        const cardData: Card[] = await response.json();
+        if (cardData) {
+          setCards(cardData);
+          const numCards = cardData.length;
+          setRadius(Math.round(width / 2 / Math.tan(Math.PI / numCards)));
+          setTheta(360 / numCards);
+        }
+      } catch (e) {
+        console.log("Failed to fetch cards", e);
+      }
+    };
+    fetchCards();
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--card-angle",
+      `${carouselAngle}deg`
+    );
+  }, [carouselAngle]);
+
+  // function rotateCarousel() {
+  //   "translateZ(" + -radius + "px) " + "rotateY" + "(" + angle + "deg)";
+  // }
+
+  const prevHandler = () => {
+    setCarouselAngle((prevAngle) => prevAngle + theta);
+    setCardAngle((prev) => prev - theta);
+  };
+
+  const nextHandler = () => {
+    setCarouselAngle((prevAngle) => prevAngle - theta);
+    setCardAngle((prev) => prev + theta);
+  };
   return (
-    <div className="carousel-container">
-      <div className="carousel relative w-full h-64">
-        {cards.map((card: Card, idx: number) => (
-          <div
-            className="carousel-item absolute flex flex-col items-center"
-            style={{
-              transform: `rotateY(${
-                idx * (360 / numCards)
-              }deg) translateZ(50px) rotateY(${-idx * (360 / numCards)}deg)`
-            }}
-            key={card.name}
-          >
-            <h1>{card.name}</h1>
-            <h2>{card.price}</h2>
-            <Image src={card.img} width={50} height={50} alt={card.name} />
-          </div>
-        ))}
+    <>
+      <div
+        className={`h-[140px] relative [perspective:900px] [perspective-origin:center_-60%] flex pt-16`}
+        style={{ width: width }}
+      >
+        <div
+          className="w-full h-full absolute [transform-style:preserve-3d] duration-500"
+          style={{
+            transform: `translateZ(${200}px) rotateY(${carouselAngle}deg)`
+          }}
+        >
+          {cards &&
+            cards.map((card: Card, idx: number) => (
+              <div
+                className={`absolute w-[190px] h-[120px] left-[10px] top-[10px] duration-500 border-none rounded-md hover-effect`}
+                style={{
+                  transform: `rotateY(${
+                    idx * theta
+                  }deg) translateZ(${radius}px) rotateY(${
+                    -idx * theta
+                  }deg) rotateY(${cardAngle}deg)`
+                }}
+                key={card.name}
+              >
+                <Image
+                  src={card.img}
+                  width={300}
+                  height={300}
+                  alt={card.name}
+                  className="w-full h-full"
+                />
+              </div>
+            ))}
+        </div>
       </div>
-    </div>
+      <div className="w-full flex justify-between mt-[400px] px-40">
+        <button className="w-20 bg-white" onClick={prevHandler}>
+          Previous
+        </button>
+        <button className="w-20 bg-white" onClick={nextHandler}>
+          Next
+        </button>
+      </div>
+    </>
   );
 }

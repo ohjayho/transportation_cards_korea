d991b44d04ec050dff4a9b0cf7c0be1c432dc3df
Jay Ho Oh <ohjayhoi@gmail.com>
2024-09-26 01:03:12 +0900

    design: organize card datas and prevent event bubbling from clicking a tag

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -61,24 +61,38 @@ export default function Card({
               transform: cardClick ? "rotateY(0deg)" : "rotateY(-180deg)"
             }}
           >
-            <div className="h-full border border-black rounded-md flex flex-col items-center text-[10px] p-2">
-              <h1 className="text-[12px] mb-2">{card.name}</h1>
-              <div className="flex justify-between">
-                {card.category.map((category) => (
-                  <div
-                    className="text-[8px] bg-red-400 text-center px-1 rounded-sm text-white mr-2 last:mr-0 mb-2"
-                    key={category}
-                  >
-                    {category}
-                  </div>
-                ))}
+            <div className="h-full border border-black rounded-md flex flex-col justify-between text-[10px] p-2">
+              <div>
+                <span className="w-[60%] text-[12px] mb-1">{card.name}</span>
+                <div className="flex mb-1">
+                  {card.category.map((category) => (
+                    <span
+                      className="text-[8px] bg-red-400 text-center px-1 rounded-sm text-white mr-2 last:mr-0"
+                      key={category}
+                    >
+                      {category}
+                    </span>
+                  ))}
+                </div>
               </div>
-              <h1 className="mb-2">{card.price}</h1>
-              <h1>
-                <a href={card.website} target="_blank">
-                  Website
+              <div>
+                <span className="text-[8px]">Purchase: </span>
+                <span>{card.availableAt}</span>
+              </div>
+              <div>
+                <span className="text-[8px]">Charge: </span>
+                <span>{card.funds}</span>
+              </div>
+              <div className="flex justify-end self-end">
+                <a
+                  href={card.website}
+                  target="_blank"
+                  className="text-red-500"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  More info
                 </a>
-              </h1>
+              </div>
             </div>
           </div>
         </div>

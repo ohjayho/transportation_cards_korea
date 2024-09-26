03b7d479cc83466d79b90b1d92d4b46231874494
Jay Ho Oh <ohjayhoi@gmail.com>
2024-09-26 12:42:31 +0900

    design: add background img for the title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,16 @@ import CardsCarousel from "@/components/CardsCarousel";
 export default function Home() {
   return (
     <>
-      <div className="w-full h-full flex justify-center items-center bg-[url('/bg.jpg')] bg-cover bg-center">
+      <div className="w-full h-full flex justify-center items-center bg-[url('/bg.jpg')] bg-cover bg-right-bottom overflow-hidden">
         <div className="flex flex-col items-center relative">
-          <h1 className="text-9xl font-bold text-[#ffffff]">Cards Korea</h1>
+          <div className="flex">
+            <h1 className="max-md:text-7xl text-9xl font-bold mr-10 text-white">
+              Cards
+            </h1>
+            <h1 className="max-md:text-7xl text-9xl font-bold text-transparent bg-[url('/flag.jpg')] bg-clip-text bg-center bg-">
+              Korea
+            </h1>
+          </div>
           <CardsCarousel />
         </div>
       </div>

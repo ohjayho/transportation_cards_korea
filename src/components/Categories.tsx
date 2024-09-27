f25c72ba801d72f051910c30ccb43add9c8f4b3d
Jay Ho Oh <ohjayhoi@gmail.com>
2024-09-27 18:43:04 +0900

    design: update design for better UX

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -6,20 +6,23 @@ export default function Categories({
   filterHandler: (e: MouseEvent) => void;
 }) {
   const categories = [
-    ["Transportation", "#00347870"],
-    ["Debit", "#29737370"],
-    ["City Pass", "#c60c3170"]
+    ["Transportation", "red"],
+    ["Debit", "green"],
+    ["City Pass", "blue"]
   ];
   return (
     <>
-      <div className="flex justify-between w-[300px] mt-10 text-white">
+      <div className="flex justify-between w-[370px] mt-10">
         {categories.map((category) => (
           <button
-            className="p-2 rounded-md"
-            style={{ backgroundColor: category[1] }}
+            className="inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-xl text-md font-medium border border-gray-200 bg-white text-gray-800 shadow-sm dark:bg-neutral-900 dark:border-neutral-700 dark:text-white"
             onClick={filterHandler}
             key={category[0]}
           >
+            <span
+              className="size-1.5 inline-block rounded-full "
+              style={{ backgroundColor: category[1] }}
+            ></span>
             {category[0]}
           </button>
         ))}

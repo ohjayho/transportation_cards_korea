11c79730f058c7cc4e10588c85ca40a3821973ea
Jay Ho Oh <ohjayhoi@gmail.com>
2024-10-07 17:12:32 +0900

    chore: add a category for Korea tour card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div className="w-full h-full flex flex-col justify-center items-center bg-[url('/bg.jpg')] bg-cover bg-right-bottom overflow-x-hidden">
-        <div className="h-[80%] flex flex-col items-center relative mt-28 max-sm:mt-10 mb-5">
+        <div className="w-full h-[80%] flex flex-col items-center relative mt-28 max-sm:mt-10 mb-5">
           <div className="flex">
             <h1 className="max-sm:text-6xl max-md:text-7xl text-9xl font-bold mr-[40px] max-md:mr-[20px] text-white">
               Cards
--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -8,14 +8,15 @@ export default function Categories({
   const categories = [
     ["Transportation", "red"],
     ["Debit", "green"],
-    ["City Pass", "blue"]
+    ["City Pass", "blue"],
+    ["Discounts", "purple"]
   ];
   return (
     <>
-      <div className="flex justify-between w-[370px] mt-10">
+      <div className="flex justify-between mt-10 max-sm:px-4 gap-2.5">
         {categories.map((category) => (
           <button
-            className="inline-flex items-center gap-x-1.5 py-1.5 px-3 rounded-xl text-md font-medium border border-gray-200 bg-white text-gray-800 shadow-sm dark:bg-neutral-900 dark:border-neutral-700 dark:text-white"
+            className="inline-flex items-center gap-x-1.5 py-1.5 px-3 max-sm:px-1.5 rounded-xl text-md font-medium border border-gray-200 bg-white text-gray-800 shadow-sm dark:bg-neutral-900 dark:border-neutral-700 dark:text-white max-sm:text-xs"
             onClick={filterHandler}
             key={category[0]}
           >
